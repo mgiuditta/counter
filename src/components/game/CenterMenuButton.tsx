83989fc114af93a44f3d14458b0a67e7d3daccf7
GIUDITTA Matteo <matteo.giuditta@soprasteria.com>
2025-03-13 09:23:33 +0100

    refactor CenterMenuButton component, add LogoSvg, and update package.json to include react-native-svg

--- src/components/game/CenterMenuButton.tsx
+++ src/components/game/CenterMenuButton.tsx
@@ -1,33 +1,28 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import {TouchableOpacity} from "react-native";
 import styled from "styled-components/native";
-import { ThemeProps } from "@/utils/theme";
-import { MaterialIcons } from "@expo/vector-icons";
+import {ThemeProps} from "@/utils/theme";
+import LogoSvg from "@/s-components/icons/LogoSvg";
 
 interface CenterMenuButtonProps {
     onPress: () => void;
 }
 
-const CenterButtonContainer = styled(TouchableOpacity)(({ theme }: ThemeProps) => ({
+const CenterButtonContainer = styled(TouchableOpacity)(({theme}: ThemeProps) => ({
     position: "absolute",
     alignSelf: "center",
     top: "50%",
-    zIndex: 3,
-    backgroundColor: theme.colors.primary,
+    backgroundColor: theme.colors.background,
     padding: theme.spacing.md,
     borderRadius: 50,
+    display: "flex",
+    justifyContent: "center",
 })) as unknown as typeof TouchableOpacity;
 
-const MenuIcon = styled(MaterialIcons).attrs(({ theme }: ThemeProps) => ({
-    name: "menu",
-    size: theme.fontSizes.xl,
-    color: theme.colors.background,
-}))``;
-
-const CenterMenuButton: React.FC<CenterMenuButtonProps> = ({ onPress }) => {
+const CenterMenuButton: React.FC<CenterMenuButtonProps> = ({onPress}) => {
     return (
-        <CenterButtonContainer onPress={onPress}>
-            <MenuIcon />
+        <CenterButtonContainer style={{transform: [{translateY: -35}]}} onPress={onPress}>
+
         </CenterButtonContainer>
     );
 };
